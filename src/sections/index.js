0cfbc75950166f8224cbdfda7dfa0e6dc1503153
drvcodenta <dhrtr.36@gmail.com>
2023-10-21 15:00:15 +0530

    Update_New_files

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReview from "./CustomerReview";
+import Subscribe from "./Subscribe";
+
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    Footer,
+    SpecialOffer,
+    CustomerReview,
+    Subscribe
+    };
