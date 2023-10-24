f0bff8217b61365b1690d50a8a267ab0da8e9a90
drvcodenta <dhrtr.36@gmail.com>
2023-10-25 01:29:52 +0530

    Added Visual NavBar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,36 @@
+import { hamburger } from "../assets/icons"
+import { headerLogo } from "../assets/images"
+import { navLinks } from "../constants"
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <header className="py-8 padding-x absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="logo" width={130} height={20}></img>
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+        {navLinks.map((item) => (
+          <li key={item.label}>
+            <a
+            href={item.href}
+            className="font-montserrat leading-normal text-lg text-slate-gray"
+            >
+            {item.label}
+            </a>
+          </li>
+        ))}
+        </ul>
+        <div className="lg:hidden">
+          <img
+          src={hamburger}
+          alt="hamburger"
+          height={20}
+          width={20}
+          ></img>
+        </div>
+      </nav>
+    </header>
   )
 }
 
