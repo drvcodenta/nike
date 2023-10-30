8842946b0b0590cbf112838b8c166836cf9ed745
drvcodenta <dhrtr.36@gmail.com>
2023-10-30 18:52:46 +0530

    Add Hero Section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,51 @@
+import { arrowRight } from "../assets/icons"
+import { bigShoe1 } from "../assets/images"
+import Button from "../components/Button"
+import { statistics } from "../constants"
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section
+      id='home'
+      className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
+    >
+    <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
+    <p className='text-xl font-montserrat text-coral-red'>
+          Our Summer collections
+    </p>
+
+    <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
+    <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
+            The New Arrival
+    </span>
+    <br />
+    <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+    </h1>
+    <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Get Started with New styles of Nike Shoes which provide Comfortable quality
+    and innovation for your Active Life</p>
+
+    <Button label="Shop now" iconURL={arrowRight}/>
+
+    <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+      {statistics.map((stat,index) => (
+        <div key={index}>
+          <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+          <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
+        </div>
+      ))}
+    </div>
+    </div>
+
+    <div className="relative flex flex-1 justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+      <img
+        src={bigShoe1}
+        alt="Shoe Collection"
+        width={500}
+        height={500}
+        className="object-contain relative z-10"
+      />
+    </div>
+    </section>
   )
 }
 
