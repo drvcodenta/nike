2e9fa2c440b9811e9f658ab3cfdd891d4d7a5d28
drvcodenta <dhrtr.36@gmail.com>
2023-11-12 01:56:19 +0530

    feat: Complete Services Section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -3,7 +3,7 @@ import { services } from "../constants"
 
 const Services = () => {
   return (
-    <section>
+    <section className="flex max-container justify-center flex-wrap gap-9">
       {services.map((service) => (
         <ServiceCard key={service.label} {...service} />
       ))}
