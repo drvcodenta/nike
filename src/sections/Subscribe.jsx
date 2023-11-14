8d82c651c7903f995167a79984aa94df213c26dd
drvcodenta <dhrtr.36@gmail.com>
2023-11-14 23:25:06 +0530

    fix: Update border

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -9,7 +9,7 @@ const Subscribe = () => {
       <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">
         Sign Up for <span className="text-coral-red"> Updates </span>and Newletter
       </h3>
-      <div className='lg:max-w-[40%] w-full items-center flex max-sm:flex-col gap-5 p-2.5 sm:border-slate-gray rounded-full'>
+      <div className='lg:max-w-[40%] w-full items-center flex max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full'>
         <input 
           type='text'
           placeholder="subscription@nike.com"
