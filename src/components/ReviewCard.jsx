e55e56c489255358073107723fe79810cb98f3a7
drvcodenta <dhrtr.36@gmail.com>
2023-11-14 19:25:04 +0530

    Add Customer Review Section

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,30 @@
+import { reviews } from "../constants"
+import { star } from "../assets/icons"
+
+const ReviewCard = ({imgURL, customerName, rating, feedback}) => {
+  return (
+    <div className="flex justify-center items-center flex-col">
+        <img 
+            src={imgURL}
+            alt="customer"
+            width={120}
+            height={120}
+            className="rounded-full object-cover"
+        />
+        <p className="mt-6 max-w-sm text-center info-text">{feedback}</p>
+        <div className="mt-3 flex gap-2.5">
+            <img 
+                src={star}
+                width={24}
+                height={24}
+                alt="star"
+                className=""
+            />
+            <p className="text-xl font-montserrat text-slate-gray">({rating})</p>
+        </div>
+        <h3 className="mt-1 font-palanquin text-3xl text-center font-bold">{customerName}</h3>
+    </div>
+  )
+}
+
+export default ReviewCard
